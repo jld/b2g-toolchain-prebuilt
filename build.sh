0fff55d2bcb3b49557feb6f8e2d19c1909dd3ce0
Jed Davis <jld@mozilla.com>
2013-03-05 14:27:35 -0800

    Fix assorted issues in build.sh, including failure to spell "toolchain".

--- build.sh
+++ build.sh
@@ -23,27 +23,36 @@ esac
 : ${MAKE_FLAGS:=-j$((CORE_COUNT + 2)) }
 : ${TOOLCHAIN_MAKE_FLAGS:=$MAKE_FLAGS}
 
-
+set -x
 case "${1:-all}" in
     all)
 	"$0" toolchain-4.4.3
 	;;
     toolchain-4.4.3)
 	SRCDIR=$TOPDIR/src/toolchain-4.4.3
+	PREFIX=$TOPDIR/toolchain-4.4.3
 	if [ -d "$SRCDIR" ]; then
 	    cd "$SRCDIR"
 	else
 	    mkdir -p "$SRCDIR"
 	    cd "$SRCDIR"
 	    "$REPO" $REPO_FLAGS init -u "$TOOLCHAIN_URL" -b "$TOOLCHAIN_BRANCH"
+	    NO_SYNC=
 	fi
-	"$REPO" $REPO_FLAGS sync $REPO_SYNC_FLAGS
+	if [ -z "${NO_SYNC:-}" ]; then
+	    "$REPO" $REPO_FLAGS sync $REPO_SYNC_FLAGS
+	fi
 	cd build
-	mkdir -p "$TOPDIR/toolchain"
+	rm -rf "$PREFIX"
+	mkdir "$PREFIX"
+	if [ -r Makefile ]; then
+	    # If a build is interrupted it can break the next build.
+	    make distclean || true
+	fi
 	LINARO_BUILD_EXTRA_CONFIGURE_FLAGS=-disable-graphite \
 	    LINARO_BUILD_EXTRA_MAKE_FLAGS=$TOOLCHAIN_MAKE_FLAGS \
 	    ./linaro-build.sh --with-gcc=gcc-4.4.3 \
-	    --prefix="$TOPDIR/toolchian-4.4.3"
+	    --prefix="$PREFIX"
 	;;
 
     *)
