326b10f977c9dd4db582a795b87b75c9ea686820
Jed Davis <jld@mozilla.com>
2013-03-04 18:45:41 -0800

    Import GCC 4.4.3 toolchain, previously built manually

---
+++ toolchain-4.4.3/lib/gcc/arm-eabi/4.4.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = ".././../gcc/gcc-4.4.3/configure --prefix=/tmp/android-toolchain-eabi --host=x86_64-linux-gnu --build=x86_64-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --with-gmp=/home/jld/src/linaro-toolchain/build/temp-install --with-mpfr=/home/jld/src/linaro-toolchain/build/temp-install --disable-libssp --disable-nls --disable-libmudflap --disable-shared --disable-sjlj-exceptions --disable-libquadmath --disable-libitm --disable-libatomic --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --prefix=/tmp/android-toolchain-eabi --disable-docs --disable-nls --with-gcc-version=4.4.3 --with-binutils-version=current --with-gmp-version=current --with-mpfr-version=3.1.1 --with-mpc-version=current --disable-graphite --target=arm-eabi --enable-threads --enable-tls --disable-libgomp --disable-libstdc__-v3 --disable-libitm --disable-libatomic";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" } };
