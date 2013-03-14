73d1c35fad1a645908ae6dcc55a433379d55b4ac
Jed Davis <jld@mozilla.com>
2013-03-14 16:34:08 -0700

    Build gold from the 20100303 tree instead of trying to fix the 2.20.1.

--- build.sh
+++ build.sh
@@ -144,7 +144,7 @@ case "${1:-all}" in
 	    # If a build is interrupted it can break the next build.
 	    make $TOOLCHAIN_MAKE_FLAGS distclean || true
 	fi
-	LINARO_BUILD_EXTRA_CONFIGURE_FLAGS=-disable-graphite \
+	LINARO_BUILD_EXTRA_CONFIGURE_FLAGS="--disable-graphite --with-gold-version=20100303" \
 	    LINARO_BUILD_EXTRA_MAKE_FLAGS=$TOOLCHAIN_MAKE_FLAGS \
 	    BINUTILS_VERSION=2.20.1 \
 	    ./linaro-build.sh --with-gcc=gcc-4.4.3 \
