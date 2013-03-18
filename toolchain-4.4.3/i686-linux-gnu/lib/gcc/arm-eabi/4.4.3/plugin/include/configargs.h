06736002379c345214553889487b4cf026a38a70
Jed Davis <jld@mozilla.com>
2013-03-18 23:11:22 +0000

    Precompile the toolchain for i686

---
+++ toolchain-4.4.3/i686-linux-gnu/lib/gcc/arm-eabi/4.4.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = ".././../gcc/gcc-4.4.3/configure --prefix=/home/jld/src/b2g-toolchain-prebuilt/toolchain-4.4.3/i686-linux-gnu --host=i686-unknown-linux-gnu --build=i686-unknown-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --with-gmp=/home/jld/src/b2g-toolchain-prebuilt/src/toolchain-4.4.3/build/temp-install --with-mpfr=/home/jld/src/b2g-toolchain-prebuilt/src/toolchain-4.4.3/build/temp-install --disable-libssp --disable-nls --disable-libmudflap --disable-shared --disable-sjlj-exceptions --disable-libquadmath --disable-libitm --disable-libatomic --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --prefix=/home/jld/src/b2g-toolchain-prebuilt/toolchain-4.4.3/i686-linux-gnu --disable-docs --disable-nls --with-gcc-version=4.4.3 --with-binutils-version=2.20.1 --with-gmp-version=current --with-mpfr-version=3.1.1 --with-mpc-version=current --disable-graphite --with-gold-version=20100303 --target=arm-eabi --enable-threads --enable-tls --disable-libgomp --disable-libstdc__-v3 --disable-libitm --disable-libatomic";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" } };
